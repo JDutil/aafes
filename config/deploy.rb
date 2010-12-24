c0663258b5d8139add23020b428d785d69e3da08
Jeff Dutil <jdutil21@gmail.com>
2010-12-24 15:13:34 -0500

    remove database.yml and update deploy to go to my server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
+require 'bundler/capistrano'
+
 set :application, "aafes"
-set :user, "root"
-set :repository, "."
+set :user, "deploy"
+set :repository, "git@github.com:jdutil/aafes.git"
 set :scm, "git"
 set :runner, user
 set :use_sudo, false
@@ -8,10 +10,9 @@ set :branch, "master"
 set :deploy_via, :copy
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
-set :deploy_to, "/home/#{application}"
+set :deploy_to, "/home/#{user}/#{application}"
 set :chmod755, %w(log public public/stylesheets public/assets tmp)
-#set :group_writable, false
-set :server_hostname, 've.qnfzmmzr.vesrv.com'
+set :server_hostname, 'burlingtonwebapps.com'
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
@@ -21,7 +22,6 @@ role :app, server_hostname
 role :db, server_hostname, :primary => true
 
 after "deploy:update_code", "deploy:cleanup"
-after "deploy:symlink", 'deploy:bundler'
 
 namespace :deploy do
 
@@ -29,13 +29,7 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-    #`curl -s http://native-apparel.com  $2 > /dev/null`
-  end
-  
-
-  desc "run bundler"
-  task :bundler, :roles => :app do
-    run "cd #{release_path} && bundle install"
+    `curl -s http://aafes.burlingtonwebapps.com  $2 > /dev/null`
   end
 
 end
